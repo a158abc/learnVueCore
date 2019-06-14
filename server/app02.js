23b3f7a5c8c1aa8bf632f6298827cc286644266c
a158abc <a158abc@163.com>
2019-06-14 15:53:02 +0800

    【chg】动画

--- server/app02.js
+++ server/app02.js
@@ -48,6 +48,13 @@ app.all('*',function (req, res, next) {
   }
 });
 
+//请求响应函数
+var response_not_finded = function(res, code) {
+  //console.log('not finded');
+  res.type('text/json');
+  res.send({success: code});
+};
+
 app.get('/api/getallbrand', function (req, res) {
     var queryClause = {};
     queryClause["order"] = [['id','ASC'], ['name','ASC']];
@@ -66,15 +73,83 @@ app.get('/api/getallbrand', function (req, res) {
     });
 });
 
-var idcount = 3;
 app.post('/api/addbrand',function(req,res){
     var name = req.body.name;
+    //console.log( new Date().format('yyyy-MM-dd HH:mm:ss'));
     Brand.create({
-     // ID: idcount++,
+     // ID: idcount++, //ID为null，数据库自增
       NAME: name,
-      CTIME: '2016-01-22 18:37:22'
-  });
+      CTIME: new Date().format('yyyy-MM-dd HH:mm:ss')
+    });
+    var result = { success: 0, result:"" };
+    res.type('text/json');
+    res.send(result);
 });  
+
+app.post('/api/delbrand',function(req,res){
+  var index = req.body.delid;
+  Brand.destroy({
+    where: { 
+       id:index
+    }
+  });
+  var result = { success: 0, result:"" };
+  res.type('text/json');
+  res.send(result);
+}); 
+
+/** 
+ * 时间对象的格式化
+ * @params string format 格式：参考这个：http://element-cn.eleme.io/#/zh-CN/component/date-picker#ri-qi-ge-shi
+ * @example new Date().format('yyyy-M-d H:m:s');
+ * @return string '2018-05-05 05:05:05';
+ */
+Date.prototype.format = function (format) {
+	var year = this.getFullYear();
+	var month = this.getMonth() + 1;
+	var day = this.getDate();
+	var hours = this.getHours();
+	var minutes = this.getMinutes();
+	var seconds = this.getSeconds();
+	
+	format = format.replace('yyyy', year);
+	
+	if ( format.indexOf('MM') !== -1 ) {
+		format = format.replace('MM', month < 10 && '0' + month || month);
+    } else {
+		format = format.replace('M', month);
+    }
+	if ( format.indexOf('dd') !== -1 ) {
+		format = format.replace('dd', day < 10 && '0' + day || day);
+    } else {
+		format = format.replace('d', day);
+    }
+	if ( format.indexOf('HH') !== -1 ) {
+		format = format.replace('HH', hours < 10 && '0' + hours || hours);
+    } else {
+		format = format.replace('H', hours);
+    }
+	if ( format.indexOf('mm') !== -1 ) {
+		format = format.replace('mm', minutes < 10 && '0' + minutes || minutes);
+    } else {
+		format = format.replace('m', minutes);
+	}
+	if ( format.indexOf('ss') !== -1 ) {
+		format = format.replace('ss', seconds < 10 && '0' + seconds || seconds);
+    } else {
+		format = format.replace('s', seconds);
+    }
+    return format;
+}
+
+/**
+ * 加减月份，配合format使用，自动计算增加、减去整数后的日期
+ * @params integer num 整数，接受负数
+ */
+Date.prototype.inCreaseMonth = function (num) {
+  this.setMonth(this.getMonth() + num);
+  return this
+}
 
 //启动服务
 var server = app.listen(3000, function () {
